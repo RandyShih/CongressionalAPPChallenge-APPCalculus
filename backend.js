f3d1ad2d5380cb42ca544642c2b69f9f24fb8d95
RandyShih <randyshih1201@gmail.com>
2025-09-01 16:23:48 -0700

    Testing

--- backend.js
+++ backend.js
@@ -1,3 +1,7 @@
 function siteRedirect() {
-    window.location.href = "subPages/lessonsPage.html";
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQn1CCsHeMndLWWdk6ilUlblrh4VHgvfa_Ap8azPtA-L3tww/viewform";
+}
+
+function pageRedirect(page) {
+    window.location.href = page
 }
