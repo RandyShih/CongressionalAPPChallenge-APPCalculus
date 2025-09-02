16bdbc428da0d3088d9992fd52eff42783c7424f
RandyShih <randyshih1201@gmail.com>
2025-09-02 05:59:45 -0700

    testing

--- main/backend.js
+++ main/backend.js
@@ -1,5 +1,16 @@
+export default {
+    async fetch(request, env) {
+        const {pathname} = new URL(request.url);
+        console.log(pathname);
+    }
+} 
+
+
+
+
 function siteRedirect() {
     window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQn1CCsHeMndLWWdk6ilUlblrh4VHgvfa_Ap8azPtA-L3tww/viewform";
+    alert(env.testing);
 }
 
 function pageRedirect(page) {
