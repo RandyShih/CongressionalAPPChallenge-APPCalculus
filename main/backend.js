356486f6d30a455920e0b6e43b09d1f56f9fdcaf
RandyShih <randyshih1201@gmail.com>
2025-09-02 06:47:15 -0700

    awd

--- main/backend.js
+++ main/backend.js
@@ -1,15 +1,6 @@
-export default {
-    async fetch(request, env) {
-        const {pathname} = new URL(request.url);
-        console.log(pathname);
-    }
-};
-
-
-
 
 function siteRedirect() {
-    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQn1CCsHeMndLWWdk6ilUlblrh4VHgvfa_Ap8azPtA-L3tww/viewform";
+    /*window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQn1CCsHeMndLWWdk6ilUlblrh4VHgvfa_Ap8azPtA-L3tww/viewform";*/
     alert(env.testing);
 }
 
