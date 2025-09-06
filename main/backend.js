a2c60b9429a714fb0378b269ab7d15e64520c628
RandyShih <randyshih1201@gmail.com>
2025-09-06 16:49:06 -0700

    Testing

--- main/backend.js
+++ main/backend.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+const mainAPIKey = process.env.testAPI;
+
+
 function siteRedirect() {
     window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQn1CCsHeMndLWWdk6ilUlblrh4VHgvfa_Ap8azPtA-L3tww/viewform";
 }
